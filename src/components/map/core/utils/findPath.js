b6da927c2e67280f866e0dc59db2e9561dd3aa8a
Dmitry Steblyuk <dmitry.steblyuk@gmail.com>
2016-09-19 09:59:02 +0300

    (5) path finder first version implemented

---
+++ src/components/map/core/utils/findPath.js
@@ -0,0 +1,153 @@
+!function() {
+
+window.flatworld.utils.findPath = findPath;
+
+// Any hexagon grid can be represented as a square grid
+// with intersections in hexagons centers (easy to see when you connect them).
+// The only difference is that there're 6 allowed directions where unit can move
+// from one intersection (hexagon center) to another in one step.
+// Imagine we have coordinate axis with Y-axis pointing to top-left corner
+// and X-axis pointing to top-right-corner.
+// Thus, it will take two steps to go in direction [1, 1] and [-1, -1]
+// and only one step to go in directions [1, 0], [1, -1], [0, -1], [-1, 0], [0, 1].
+// Now that we have set ground for the problem let's begin.
+
+/**
+ * Finds shortest route on a hexagon grid
+ * @param  {number} x - start x-coordinate
+ * @param  {number} y - start y-coordinate
+ * @param  {number} xDest - destination x-coordinate
+ * @param  {number} yDest - destination y-coordinate
+ * @param  {number} maxSteps - maximum allowed number of steps (must be at least 1)
+ * @param  {(x: number, y: number) => boolean} isBlocked - function that returns true if coordinate is blocked
+ * @return {number[]} - path coordinates from start to destination (including starting point)
+ */
+
+function findPath(xStart, yStart, xDest, yDest, maxSteps, isBlocked) {
+    for (let i = 0; i < 5; i++) {
+        if (!isInteger(arguments[i])) {
+            throw new Error(`argument #${i} must be an integer: ${arguments[i]}`);
+        }
+    }
+    if (maxSteps <= 0) {
+        throw new Error(`maxSteps must be greater or equal to 1: ${maxSteps}`);
+    }
+    if (xStart == xDest && yStart == yDest) {
+        throw new Error(`starting and destination points must be different: ${xStart}, ${yStart}`);
+    }
+    const tramped = [xStart, yStart, 1];
+    
+    let counter = 0;
+    let d = Date.now();
+    
+    const path = recursiveAlg(xStart, yStart, maxSteps + 1, [xStart, yStart]);
+    
+    console.log(`${Date.now() - d} ms`, `${counter} oper`, `${maxSteps} cells`,
+        `${counter / maxSteps / Math.log(maxSteps)} coeff`);
+    
+    return path;
+    
+    function recursiveAlg(xLast, yLast, maxPathLen, currPath) {
+        counter++;
+        let resPath = null;
+        let maxLen = maxPathLen;
+        const directions = getBestDirections(xDest - xLast, yDest - yLast);
+        
+        for (let i = 0; i < directions.length; i++) {
+            const x = xLast + directions[i][0];
+            const y = yLast + directions[i][1];
+            
+            if (x === xDest && y === yDest) {
+                currPath.push(x, y);
+                return currPath;
+            }
+            
+            const len = currPath.length / 2;
+            const remainingSteps = maxLen - len;
+            const noWay = !remainingSteps ||
+                getMinSteps(xDest - x, yDest - y) > remainingSteps ||
+                isBlocked(x, y) ||
+                isTramped(x, y, len);
+            
+            if (!noWay) {
+                const nextPath = currPath.concat([x, y]);
+                const finalPath = recursiveAlg(x, y, maxLen, nextPath);
+                if (finalPath) {
+                    resPath = finalPath;
+                    maxLen = finalPath.length / 2 - 1;
+                }
+            }
+        }
+        return resPath;
+    }
+    
+    function isTramped(x, y, len) {
+        const [index, match] = tramped.length ?
+            binarySearch(i => x - tramped[i *= 3] || y - tramped[i + 1], 0, tramped.length / 3)
+            : [0, false];
+        const j = index * 3;
+        
+        return match ?
+            tramped[j + 2] <= len || (tramped[j + 2] = len, false)
+            : (tramped.splice(j, 0, x, y, len), false);
+    }
+}
+
+function binarySearch(sortFn, i0, i1) {
+    const mid = Math.floor((i0 + i1) / 2);
+    const res = sortFn(mid);
+    
+    if (res < 0) {
+        return mid > i0 ? binarySearch(sortFn, i0, mid) : [i0, false];
+    } else if (res > 0) {
+        return mid + 1 < i1 ? binarySearch(sortFn, mid + 1, i1) : [i1, false];
+    }
+    return [mid, true];
+}
+
+function isInteger(x) {
+    return x === Math.floor(x) && isFinite(x);
+}
+
+function getBestDirections(dx, dy) {
+    return dx > 0 ?
+            (dy > 0 ? allDirections.north_east
+            : dy < 0 ?
+                (dx < -dy ? allDirections.south_south_east
+                : dx > -dy ? allDirections.south_east_east
+                : allDirections.south_east)
+            : allDirections.east)
+        : dx < 0 ?
+            (dy > 0 ?
+                (-dx < dy ? allDirections.north_north_west
+                : -dx > dy ? allDirections.north_west_west
+                : allDirections.north_west)
+            : dy < 0 ? allDirections.south_west
+            : allDirections.west)
+        : (dy > 0 ? allDirections.north
+            : dy < 0 ? allDirections.south
+            : null);
+}
+
+const allDirections = {
+    north: [[0, 1], [-1, 1], [1, 0], [1, -1], [-1, 0], [0, -1]],
+    south: [[0, -1], [1, -1], [-1, 0], [-1, 1], [1, 0], [0, 1]],
+    east: [[1, 0], [1, -1], [0, 1], [-1, 1], [0, -1], [-1, 0]],
+    west: [[-1, 0], [-1, 1], [0, -1], [1, -1], [0, 1], [1, 0]],
+    north_west: [[-1, 1], [0, 1], [-1, 0], [0, -1], [1, 0], [1, -1]],
+    south_east: [[1, -1], [0, -1], [1, 0], [0, 1], [-1, 0], [-1, 1]],
+    north_east: [[0, 1], [1, 0], [1, -1], [-1, 1], [-1, 0], [0, -1]],
+    south_west: [[0, -1], [-1, 0], [-1, 1], [1, -1], [1, 0], [0, 1]],
+    north_north_west: [[0, 1], [-1, 1], [-1, 0], [1, 0], [1, -1], [0, -1]],
+    south_south_east: [[0, -1], [1, -1], [1, 0], [-1, 0], [-1, 1], [0, 1]],
+    north_west_west: [[-1, 0], [-1, 1], [0, 1], [0, -1], [1, -1], [1, 0]],
+    south_east_east: [[1, 0], [1, -1], [0, -1], [0, 1], [-1, 1], [-1, 0]]
+};
+
+function getMinSteps(dx, dy) {
+    return dx > 0 && dy > 0 ? dx + dy
+        : dx < 0 && dy < 0 ? -dx - dy
+        : Math.max(Math.abs(dx), Math.abs(dy));
+}
+
+}();
---
+++ tests/unit/findPathSpec.js
@@ -0,0 +1,196 @@
+
+!function() {
+/* global describe, beforeEach, it, expect */
+'use strict';
+
+const findPath = window.flatworld.utils.findPath;
+
+describe('findPath', () => {
+    const falseFn = () => false;
+    
+    it('should fail', () => {
+        expect(() => findPath(0, 0, 1, 2, 0, falseFn)).toThrowError(/maxSteps/);
+        expect(() => findPath(0, '0', 1, 2, 5, falseFn)).toThrowError(/must be an integer/);
+        expect(() => findPath(0, Infinity, 1, 2, 5, falseFn)).toThrowError(/must be an integer/);
+        expect(() => findPath(4, 1, 4, 1, 100, falseFn)).toThrowError(/must be different/);
+    });
+    
+    it('should find path for simple fields without blocked cells', () => {
+        expect(findPath(1, 1, 0, 0, 10, falseFn).length).toEqual(3 * 2);
+        expect(findPath(-10, 5, 0, 0, 20, falseFn).length).toEqual(11 * 2);
+        expect(findPath(10, -5, 7, 4, 20, falseFn).length).toEqual(10 * 2);
+        expect(findPath(-10, -5, 1, 1, 20, falseFn).length).toEqual(18 * 2);
+        
+        testField(`
+            s.....
+            .+.0..
+            ..+...
+            ...+d.
+            `);
+    });
+    
+    it('now there is a block', () => {
+        testField(`
+            s.....
+            .++0..
+            ..B+..
+            ....d.
+            `);
+    });
+    
+    it('a couple of them', () => {
+        testField(`
+            ..Bd..
+            ...B+.
+            .++++.
+            s+..0.
+            `);
+    });
+    
+    it('more complicated stuff', () => {
+        testField(`
+            BBB........
+            .s.B.+d.B..
+            ..+0B+B.B..
+            ...+B+.....
+            ...+B+.B...
+            ..BB++.....
+            .B....B....
+            ....B......
+            `);
+    });
+    
+    it('too many blocks', () => {
+        testField(`
+            BBB........
+            Bs.B.Bd.B..
+            B+.B..B+B..
+            B+.B....+..
+            B.+B.BBB+B.
+            B.B+B..BB+.
+            .B..+BB.B+.
+            0....+++++.
+            `);
+    });
+    
+    it('bit longer path', () => {
+        testField(`
+            ....BBBBBB....
+            ....Bd+..0....
+            BBBBBB.+......
+            Bs....BB+.....
+            B.+.....B++++.
+            B..+.....BBBB+
+            ....++++++++++
+            `);
+    });
+    
+    it('large field', () => {
+        testField(`
+            ...xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.....
+            ...xBBBBBBBB.......................................BBBBBBBx....
+            ...Bxxx....B.......................................B.dxxxxx....
+            BBBBBBBx...B..................0....................B..+....B...
+            BsxxxxxxB...BB.....................................B...+.......
+            B.+......B....BBB..................................B....+......
+            B..+......B......BBBBBBBBBBBBBBBBBB................B.....+.....
+            B...+......BB...............B......................B......+....
+            B....+.......B...............B.....................B.......+...
+            B.....+......B................B....................B........+..
+            .......+.....B.................B...........BBBBBBBBB.........+.
+            ........+....B..................B...BBBBBBBB..................+
+            .........+.....B.................BB...........................+
+            ..........+........................BBBBBBBBBBBBBBBBBBBBBBBBBBB+
+            ...........++++++++++++++++++++++++++++++++++++++++++++++++++B+
+            .............................................................++
+            `, 74);
+    });
+    
+    it('no way', () => {
+        testField(`
+            ...............................................................
+            ....BBBBBBBB.......................................BBBBBBB.....
+            ...B.......B.......................................B.d....B....
+            BBBBBBB....B..................0....................B.......B...
+            Bs......B...BB.....................................BBBBBBBBB...
+            B........B....BBB..................................B...........
+            B.........B......BBBBBBBBBBBBBBBBBB................B...........
+            B..........BB...............B......................B...........
+            B............B...............B.....................B...........
+            B............B................B....................B...........
+            .............B.................B...........BBBBBBBBB...........
+            .............B..................B...BBBBBBBB...................
+            ...............B.................BB............................
+            ...................................BBBBBBBBBBBBBBBBBBBBBBBBBBB.
+            .............................................................B.
+            ...............................................................
+            `, 0);
+    });
+});
+
+function testField(field, total) {
+    const width = field.trim().indexOf('\n');
+    if (!new RegExp(`^\\s*([sdB0\\.\\+x]{${width}}(\\n\\s*|$))+$`).test(field) ||
+            (field.match(/s/g) || []).length !== 1 ||
+            (field.match(/d/g) || []).length !== 1 ||
+            (field.match(/0/g) || []).length !== 1) {
+        throw new Error(`incorrect field: ${field}`);
+    }
+    field = field.replace(/\s+/g, '');
+    
+    const height = field.length / width;
+    const centerIndex = field.indexOf('0');
+    const startIndex = field.indexOf('s');
+    const destIndex = field.indexOf('d');
+    
+    const dx0 = centerIndex % width;
+    const dy0 = Math.floor(centerIndex / width);
+    const xStart = startIndex % width - dx0;
+    const yStart = dy0 - Math.floor(startIndex / width);
+    const xDest = destIndex % width - dx0;
+    const yDest = dy0 - Math.floor(destIndex / width);
+    
+    // just making sure that everything is alright:
+    expect(height === Math.floor(height)).toBe(true);
+    expect(cell(xDest, yDest)).toBe('d');
+    expect(cell(xStart, yStart)).toBe('s');
+    expect(cell(0, 0)).toBe('0');
+    expect(cell(width - dx0, 0)).toBe('B');
+    expect(cell(-dx0 - 1, 0)).toBe('B');
+    expect(cell(0, dy0 + 1)).toBe('B');
+    expect(cell(0, dy0 - height)).toBe('B');
+    
+    const maxSteps = width * height;
+    const isBlocked = (x, y) => cell(x, y) === 'B';
+    
+    if (total === undefined) {
+        const Xs = field.match(/\x/g);
+        const Ps = field.match(/\+/g);
+        total = Xs && Ps ? Math.min(Xs.length, Ps.length) : (Xs || Ps || []).length;
+        total += 2;
+    }
+    
+    validatePath(findPath(xStart, yStart, xDest, yDest, maxSteps, isBlocked));
+    // now find the way back:
+    validatePath(findPath(xDest, yDest, xStart, yStart, maxSteps, isBlocked));
+    
+    function validatePath(path) {
+        for (let i = 2; i < path && path.length; i += 2) {
+            const dx = path[i - 2] - path[i];
+            const dy = path[i - 1] - path[i + 1];
+            if (dx === dy || [-1, 0, 1].indexOf(dx) < 0 || [-1, 0, 1].indexOf(dy) < 0) {
+                throw new Error(`invalid path returned: ${path}`);
+            }
+        }
+        expect(path && path.length / 2).toBe(total || null);
+    }
+    
+    function cell(x, y) {
+        x += dx0;
+        y = dy0 - y;
+        return x < 0 || x >= width || y < 0 || y >= height ? 'B'
+            : field[y * width + x];
+    }
+}
+
+}();
