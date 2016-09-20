6d70e0c74c85f24cee31f3aa4c4b4260928b9990
Dmitry Steblyuk <dmitry.steblyuk@gmail.com>
2016-09-20 23:15:39 +0300

    (5) made algorithm considerably faster by replacing stack with priority queue

--- src/components/map/core/utils/findPath.js
+++ src/components/map/core/utils/findPath.js
@@ -50,22 +50,26 @@ function findPath(xStart, yStart, xDest, yDest, maxSteps, isBlocked) {
     function bestDirectionAlg() {
         const size = maxSteps + 1;
         const visited = [];
-        const stack = [{
+        const startMinSteps = getMinSteps(xDest - xStart, yDest - yStart);
+        const queue = new PriorityQueue();
+        queue.push({
             len: 1,
             prev: null,
             x: xStart,
             y: yStart
-        }];
+        }, 0);
+        
         let resPath = null;
         let maxLen = maxSteps + 1;
         let key;
         
-        while(stack.length) {
+        while(queue.length) {
             counter++;
-            const curr = stack.pop();
+            const curr = queue.pop();
             const remainingSteps = maxLen - curr.len;
+            const minSteps = remainingSteps && getMinSteps(xDest - curr.x, yDest - curr.y);
             
-            if (remainingSteps && getMinSteps(xDest - curr.x, yDest - curr.y) <= remainingSteps) {
+            if (minSteps && minSteps <= remainingSteps) {
                 const directions = getBestDirections(xDest - curr.x, yDest - curr.y);
                 
                 for (let i = directions.length; i-- > 0; ) {
@@ -79,17 +83,21 @@ function findPath(xStart, yStart, xDest, yDest, maxSteps, isBlocked) {
                     }
                     
                     let next;
-                    const notVisited = visited[key = (x - xStart) * size + (y - yStart)] ?
-                        visited[key] > curr.len && (visited[key] = curr.len, true)
-                        : (visited[key] = curr.len, true);
-                    
-                    if (notVisited && !isBlocked(next = { len: curr.len + 1, prev: curr, x: x, y: y })) {
-                        stack.push(next);
+                    if (!isVisited(x, y, curr.len) && !isBlocked(next = { len: curr.len + 1, prev: curr, x: x, y: y })) {
+                        const loss = curr.len + minSteps - directions[i][2] - startMinSteps;
+                        queue.push(next, loss);
                     }
                 }
             }
         }
         return resPath;
+        
+        function isVisited(x, y, len) {
+            const key = (x - xStart) * size + (y - yStart);
+            return visited[key] ?
+                visited[key] <= len || (visited[key] = len, false)
+                : (visited[key] = len, false)
+        }
     }
 }
 
@@ -106,6 +114,71 @@ function pathListToArray(pathList) {
     return arr;
 }
 /*
+class PriorityQueue {
+    constructor() {
+        this.stacks = [];
+        this.losses = [];
+    }
+    
+    get length() {
+        return this.losses.length;
+    }
+    
+    pop() {
+        const loss = this.losses[this.losses.length - 1];
+        const value = this.stacks[loss].pop();
+        if (!this.stacks[loss].length) {
+            delete this.stacks[loss];
+            this.losses.pop();
+        }
+        return value;
+    }
+    
+    push(value, loss) {
+        if (this.stacks[loss]) {
+            this.stacks[loss].push(value);
+        } else {
+            this.stacks[loss] = [value];
+            const index = binarySearch(i => this.losses[i] - loss, 0, this.losses.length)[0];
+            this.losses.splice(index, 0, loss);
+        }
+    }
+}
+*/
+
+class PriorityQueue {
+    constructor() {
+        this.stacks = [];
+    }
+    
+    get length() {
+        return this.stacks.length;
+    }
+    
+    pop() {
+        const stack = this.stacks[this.stacks.length - 1];
+        const value = stack.pop();
+        if (!stack.length) {
+            this.stacks.pop();
+        }
+        return value;
+    }
+    
+    push(value, loss) {
+        const [index, match] = this.stacks.length ?
+            binarySearch(i => this.stacks[i]._loss - loss, 0, this.stacks.length)
+            : [0, false];
+        
+        if (match) {
+            this.stacks[index].push(value);
+        } else {
+            const stack = [value];
+            this.stacks.splice(index, 0, stack);
+            stack._loss = loss;
+        }
+    }
+}
+
 function binarySearch(sortFn, i0, i1) {
     const mid = Math.floor((i0 + i1) / 2);
     const res = sortFn(mid);
@@ -117,7 +190,7 @@ function binarySearch(sortFn, i0, i1) {
     }
     return [mid, true];
 }
-*/
+
 function isInteger(x) {
     return x === Math.floor(x) && isFinite(x);
 }
@@ -143,18 +216,18 @@ function getBestDirections(dx, dy) {
 }
 
 const allDirections = {
-    north: [[0, 1], [-1, 1], [1, 0], [1, -1], [-1, 0], [0, -1]],
-    south: [[0, -1], [1, -1], [-1, 0], [-1, 1], [1, 0], [0, 1]],
-    east: [[1, 0], [1, -1], [0, 1], [-1, 1], [0, -1], [-1, 0]],
-    west: [[-1, 0], [-1, 1], [0, -1], [1, -1], [0, 1], [1, 0]],
-    north_west: [[-1, 1], [0, 1], [-1, 0], [0, -1], [1, 0], [1, -1]],
-    south_east: [[1, -1], [0, -1], [1, 0], [0, 1], [-1, 0], [-1, 1]],
-    north_east: [[0, 1], [1, 0], [1, -1], [-1, 1], [-1, 0], [0, -1]],
-    south_west: [[0, -1], [-1, 0], [-1, 1], [1, -1], [1, 0], [0, 1]],
-    north_north_west: [[0, 1], [-1, 1], [-1, 0], [1, 0], [1, -1], [0, -1]],
-    south_south_east: [[0, -1], [1, -1], [1, 0], [-1, 0], [-1, 1], [0, 1]],
-    north_west_west: [[-1, 0], [-1, 1], [0, 1], [0, -1], [1, -1], [1, 0]],
-    south_east_east: [[1, 0], [1, -1], [0, -1], [0, 1], [-1, 1], [-1, 0]]
+    north: [[0, 1, 1], [-1, 1, 0], [1, 0, 0], [1, -1, -1], [-1, 0, -1], [0, -1, -1]],
+    south: [[0, -1, 1], [1, -1, 0], [-1, 0, 0], [-1, 1, -1], [1, 0, -1], [0, 1, -1]],
+    east: [[1, 0, 1], [1, -1, 0], [0, 1, 0], [-1, 1, -1], [0, -1, -1], [-1, 0, -1]],
+    west: [[-1, 0, 1], [-1, 1, 0], [0, -1, 0], [1, -1, -1], [0, 1, -1], [1, 0, -1]],
+    north_west: [[-1, 1, 1], [0, 1, 0], [-1, 0, 0], [0, -1, -1], [1, 0, -1], [1, -1, -1]],
+    south_east: [[1, -1, 1], [0, -1, 0], [1, 0, 0], [0, 1, -1], [-1, 0, -1], [-1, 1, -1]],
+    north_east: [[0, 1, 1], [1, 0, 1], [1, -1, 0], [-1, 1, 0], [-1, 0, -1], [0, -1, -1]],
+    south_west: [[0, -1, 1], [-1, 0, 1], [-1, 1, 0], [1, -1, 0], [1, 0, -1], [0, 1, -1]],
+    north_north_west: [[0, 1, 1], [-1, 1, 1], [-1, 0, 0], [1, 0, 0], [1, -1, -1], [0, -1, -1]],
+    south_south_east: [[0, -1, 1], [1, -1, 1], [1, 0, 0], [-1, 0, 0], [-1, 1, -1], [0, 1, -1]],
+    north_west_west: [[-1, 0, 1], [-1, 1, 1], [0, 1, 0], [0, -1, 0], [1, -1, -1], [1, 0, -1]],
+    south_east_east: [[1, 0, 1], [1, -1, 1], [0, -1, 0], [0, 1, 0], [-1, 1, -1], [-1, 0, -1]]
 };
 
 function getMinSteps(dx, dy) {
