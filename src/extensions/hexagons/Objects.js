ed03ae6252b3f784ecab596d834c2d513ac8e975
Janne Hyytiä <janne.hyytia@saunalahti.fi>
2017-01-12 22:08:32 +0200

    updated PIXI

--- src/extensions/hexagons/Objects.js
+++ src/extensions/hexagons/Objects.js
@@ -117,7 +117,7 @@ function calculateHexa(radius) {
   this.hitTest = function (coords) {
     const localCoords = this.toLocal(new PIXI.Point(coords.x, coords.y));
 
-    return bugFixedContains.call(this.hitArea, localCoords.x * this.scale.x, localCoords.y * this.scale.y);
+    return this.hitArea.contains(localCoords.x * this.scale.x, localCoords.y * this.scale.y);
   };
 }
 /*-----------------------
@@ -135,34 +135,6 @@ function setAndGetShape(radius) {
   }
 
   return shape;
-}
-
-/* THIS IS ONLY TEMPORARY FIX. The PIXIs own contains method had a bug introduced in v4.1.0 and
- * and that is the one npm retrieves, so I decided to add the old v4.0.0 version here as a
- * temporary fix. Can be removed after PIXI fixes it's bug. Githib ticket here:
- * https://github.com/pixijs/pixi.js/pull/3165
- */
-function bugFixedContains (x, y)
-{
-  let inside = false;
-
-  // use some raycasting to test hits
-  // https://github.com/substack/point-in-polygon/blob/master/index.js
-  const length = this.points.length / 2;
-
-  for (let i = 0, j = length - 1; i < length; j = i++)
-  {
-    const xi = this.points[i * 2], yi = this.points[i * 2 + 1],
-      xj = this.points[j * 2], yj = this.points[j * 2 + 1],
-      intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-
-    if (intersect)
-    {
-      inside = !inside;
-    }
-  }
-
-  return inside;
 }
 
 /*---------------------
